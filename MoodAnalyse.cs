19c1b0a0cae54968efada61c491d6d0be64e6fe8
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-09 16:08:32 +0530

    [tejas] add Happy or Sad Mood

---
+++ MoodAnalyse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyse
+    {
+        public string message;
+
+        public MoodAnalyse(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer");
+        }
+    }
+}
